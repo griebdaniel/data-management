25abe04665cce26bbc9637a4ef1b2e1f7ef8a658
daniel <griebdr@gmail.com>
2018-11-26 15:19:50 +0200

    redoall

--- data-management-client/src/app/services/config.ts
+++ data-management-client/src/app/services/config.ts
@@ -1,9 +1,14 @@
 const localUrl = 'http://localhost:3000/';
 const serverUrl = 'https://datamanagement.ro.lt/api/';
+const localServerUrl = 'http://localhost/api/';
+
+const serverUrl2 = 'http://35.243.194.240/api/';
 
 function getUrl() {
   // return serverUrl;
-  return localUrl;
+  // return localUrl;
+  // return localServerUrl;
+  return serverUrl2;
 }
 
 export { getUrl };
